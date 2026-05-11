853b210b4db52977589136c24ccea64811717e69
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-05-11 15:30:08 +0700

    menyertakan header jantung dan library string

--- Manusia.h
+++ Manusia.h
@@ -1,12 +1,16 @@
 #ifndef MANUSIA_H
 #define MANUSIA_H
 
+#include <iostream>
+#include <string>
+#include "jantung.h" // Sangat penting untuk menghubungkan ke class jantung
+
 using namespace std;
 
 class manusia {
 public:
     string name;
-    jantung varJantung;
+    jantung varJantung; // Inilah letak hubungan Komposisi
 
     manusia(string pName)
         : name(pName) {
