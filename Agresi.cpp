97dac4a591edee63c979feffd756f72fdf1b81e0
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-05-11 15:11:19 +0700

    memasukkan logika agresi

--- Agresi.cpp
+++ Agresi.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
-using namespace std;
-
 #include "anak.h"
 #include "ibu.h"
 
-int main() { //agregasi
+using namespace std;
+
+int main() {
+    // Membuat objek Ibu
     ibu* varIbu = new ibu("dini");
     ibu* varIbu2 = new ibu("novi");
+
+    // Membuat objek Anak
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
+    // Menambahkan hubungan (Agregasi)
     varIbu->tambahAnak(varAnak1);
     varIbu->tambahAnak(varAnak2);
     varIbu2->tambahAnak(varAnak3);
-    varIbu2->tambahAnak(varAnak1);
+    varIbu2->tambahAnak(varAnak1); // Satu anak bisa punya banyak ibu dalam kode ini
 
+    // Cetak hasil
     varIbu->cetakAnak();
     varIbu2->cetakAnak();
 
+    // Hapus objek dari memori
     delete varIbu;
     delete varIbu2;
     delete varAnak1;
     delete varAnak2;
     delete varAnak3;
-
-    system("pause");
 
     return 0;
 }
